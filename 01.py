0f94aec01fdfde19277430afaf2d34a4809da5ea
jkimtokyo <jkimtokyo@gmail.com>
2016-09-30 00:24:57 +0900

    not very much

--- 01.py
+++ 01.py
@@ -4,10 +4,17 @@ from collections import Counter
 c = Counter({0, 1, 2, 3})
 print(c)
 
-
 for x in range(10):
     if x == 2:
         continue
     if x == 8:
         break
     print(x)
+
+numbers = [x for x in range(5) if x %2 ==0]
+print(numbers)
+
+import random
+four = [random.random() for _ in range(2)] #random.random()은 0과 1 사이 난수 생성 모듈, 여기서 레인지는 난수의 갯수가 된다.
+print(four)
+
