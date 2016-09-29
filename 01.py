1645bf395417dbb6a5ad11312a1dc95200ba3e4f
jkimtokyo <jkimtokyo@gmail.com>
2016-09-29 15:50:16 +0900

    continue

--- 01.py
+++ 01.py
@@ -1,2 +1,13 @@
 #config=utf-8
 
+from collections import Counter
+c = Counter({0, 1, 2, 3})
+print(c)
+
+
+for x in range(10):
+    if x == 2:
+        continue
+    if x == 8:
+        break
+    print(x)
